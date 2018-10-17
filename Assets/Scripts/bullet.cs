c09b68774aacb5c0b7e0ce9e4a7660913dca42ca
Liish <Merlierjo@gmail.com>
2018-10-17 16:44:23 +0200

    Added bullet + conf bullet

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    Transform transform;
+    Rigidbody2D rigidbody;
+
+    [SerializeField] float speed;
+
+    Vector2 force;
+
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        force = new Vector2(0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody.MovePosition(rigidbody.position + force * Time.fixedDeltaTime);
+    }
+
+    public void setShot(float x, float y, float speed)
+    {
+        this.speed = speed;
+        force = new Vector2(x, y);
+    }
+    
+}
