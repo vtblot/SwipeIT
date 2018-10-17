b61ae681b2cb55c6c08938d5924c3471727467a6
Niko <nicolas.blanchard95@gmail.com>
2018-10-17 14:48:55 +0200

    Deplacements P1 P2

---
+++ Assets/Scripts/Player1Controler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controler : MonoBehaviour {
+    //[SerializeField] private Transform m_SpawnPoint;
+    [SerializeField] private float m_Speed;
+
+    private Vector2 movement;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+
+        movement = new Vector2(
+                m_Speed * inputX,
+                m_Speed * inputY
+            );
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Transform>().Translate(movement);
+
+    }
+}
