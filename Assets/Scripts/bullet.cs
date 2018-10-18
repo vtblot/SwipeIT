f67f422e3d1c2a896d9967c85e59904dfe8b0d70
Goubi <vtblot@gmail.com>
2018-10-18 11:29:03 +0200

    fire

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -2,32 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour {
 
-    Transform transform;
-    Rigidbody2D rigidbody;
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.tag.Equals("Ground"))
+		{
+			Destroy(gameObject, 0);
+		}
+	}
 
-    [SerializeField] float speed;
-
-    Vector2 force;
-
-	// Use this for initialization
-	void Start () {
-        transform = GetComponent<Transform>();
-        rigidbody = GetComponent<Rigidbody2D>();
-
-        force = new Vector2(0, 0);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        rigidbody.MovePosition(rigidbody.position + force * Time.fixedDeltaTime);
-    }
-
-    public void setShot(float x, float y, float speed)
-    {
-        this.speed = speed;
-        force = new Vector2(x, y);
-    }
-    
 }
