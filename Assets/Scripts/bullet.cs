2eb2e388259765f8934d637cc61103fca4f60bed
Goubi <vtblot@gmail.com>
2018-10-18 13:03:33 +0200

    lol

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -12,4 +12,30 @@ public class Bullet : MonoBehaviour {
 		}
 	}
 
+<<<<<<< HEAD
+=======
+    [SerializeField] float speed;
+
+    Vector2 force;
+
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        force = new Vector2(0.7f, 0.7f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody.MovePosition(rigidbody.position + force * 3 * Time.fixedDeltaTime);
+    }
+
+    public void setShot(float x, float y, float speed)
+    {
+        this.speed = speed;
+        force = new Vector2(x, y);
+    }
+    
+>>>>>>> d22e562b129056c03d9dadff0b5a030cbc09dad5
 }
