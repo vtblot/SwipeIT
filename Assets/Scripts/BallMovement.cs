a3a0527697e6ae2bf7a22f791cb6b12eb7a28363
Niko <nicolas.blanchard95@gmail.com>
2018-10-19 10:31:38 +0200

    BeachBall

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private Transform[] spawnPoints;
+    [SerializeField] float speed;
+    private bool isConf = false;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (isConf)
+        {
+            Debug.Log("Dans");
+            int endPoint = 0;
+            int spawnPointIndex = 0;
+            int count = 0;
+            foreach(Transform sp in spawnPoints)
+            {
+                if (sp == transform)
+                {
+                    spawnPointIndex = count;
+                }
+
+                count++;
+            }
+
+            if (spawnPointIndex + 4 < spawnPoints.Length)
+            {
+                endPoint = spawnPointIndex + 4;
+            }
+            else
+            {
+                endPoint = spawnPointIndex - 4;
+            }
+
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(spawnPoints[endPoint].position.x, spawnPoints[endPoint].position.y), speed * Time.deltaTime);
+            float dist = Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(spawnPoints[endPoint].position.x, spawnPoints[endPoint].position.y));
+            if (dist < 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        
+    }
+
+    public void Conf(Transform[] transforms)
+    {
+        spawnPoints = transforms;
+        isConf = true;
+        Debug.Log("Conf");
+    }
+}
---
+++ Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour {
+
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] GameObject ball;
+    [SerializeField] int spawnTime;
+    
+    private int spawnPointIndex;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnBall());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnBall()
+    {
+        spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        GameObject enn = Instantiate(ball, spawnPoints[spawnPointIndex]);
+        enn.GetComponent<BallMovement>().Conf(spawnPoints);
+        yield return new WaitForSeconds(spawnTime);
+    }
+}
